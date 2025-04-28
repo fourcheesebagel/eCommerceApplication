ae28fc684f75213f46c9a02461f77ffe7f03e0f6
fourcheesebagel <112727798+fourcheesebagel@users.noreply.github.com>
2025-04-28 09:15:34 -0600

    creating the backend API

---
+++ eCommerceApp.Application/DTOs/Product/CreateProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace eCommerceApp.Application.DTOs.Product
+{
+    public class CreateProduct : ProductBase // : between is an inheritance meaning it will take all the ProductBase properties into the class its being injected into
+    {
+
+    }
+
+    public class CreateCategory : CategoryBase
+    {
+    }
+
+    public class UpdateCategory : CategoryBase
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class CategoryBase
+    {
+        public string? Name { get; set; }
+    }
+}
---
+++ eCommerceApp.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceApp.Domain.Entities
+{
+    public class Category
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public ICollection<Product> Products { get; set; } //establishes relationship with Product one category to many products
+
+    }
+    //CTRL+. -> moves class to a new file
+}

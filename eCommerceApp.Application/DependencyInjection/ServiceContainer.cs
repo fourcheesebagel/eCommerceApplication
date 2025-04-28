801aecbeccef404858b752c6fb01689e17ca727c
fourcheesebagel <112727798+fourcheesebagel@users.noreply.github.com>
2025-04-28 10:42:13 -0600

    adding mapping service to the application

---
+++ eCommerceApp.Application/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,19 @@
+﻿using eCommerceApp.Application.Mapping;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Application.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingConfig));
+            return services;
+        }
+    }
+}

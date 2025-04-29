44d1eeb48e11e3bdfc00e81c5854a748db95ac38
fourcheesebagel <112727798+fourcheesebagel@users.noreply.github.com>
2025-04-29 11:24:39 -0600

    creating the interface for the token management and user management 'IMethods' to our domain, for use to talk between application to server

---
+++ eCommerceApp.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eCommerceApp.Domain.Entities.Identity
+{
+    public class AppUser : IdentityUser //Creating the User Object, using the Identity user properties to be consumed
+    {
+        public string FullName { get; set; } = string.Empty;
+    }
+}
--- eCommerceApp.Infrastructure/DependencyInjection/ServiceContainer.cs
+++ eCommerceApp.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -1,15 +1,20 @@
 ﻿using eCommerceApp.Application.Services.Interfaces.Logging;
 using eCommerceApp.Domain.Entities;
+using eCommerceApp.Domain.Entities.Identity;
 using eCommerceApp.Domain.Interfaces;
 using eCommerceApp.Infrastructure.Data;
 using eCommerceApp.Infrastructure.Middleware;
 using eCommerceApp.Infrastructure.Repositories;
 using eCommerceApp.Infrastructure.Services;
 using EntityFramework.Exceptions.SqlServer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 namespace eCommerceApp.Infrastructure.DependencyInjection
 {
     public static class ServiceContainer //Basically the Program.CS from school
@@ -29,6 +34,42 @@ namespace eCommerceApp.Infrastructure.DependencyInjection
             services.AddScoped<IGeneric<Product>, GenericRepository<Product>>();
             services.AddScoped<IGeneric<Category>, GenericRepository<Category>>();
             services.AddScoped(typeof(IAppLogger<>), typeof(SerilogLoggerAdapter<>));
+
+            //This sets up user registration and log in RULES
+            services.AddDefaultIdentity<AppUser>(options => //FIRST allow normal sign up through the app itself, no social logins ie., google, microsoft, using the AppUser class which consumes microsooft identity user class
+            {
+                options.SignIn.RequireConfirmedEmail = true; //This will makje sure that the email of the account is confirmed before being able to log in
+                options.Tokens.EmailConfirmationTokenProvider = TokenOptions.DefaultEmailProvider; //uses standard email system to send confirmation links
+                options.Password.RequireDigit = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 1;
+            })
+                .AddRoles<IdentityRole>() //Adds support for roles
+                .AddEntityFrameworkStores<AppDbContext>(); //THIS SAves the user to the Database
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; //Uses JWT Token
+                options.DefaultChallengeScheme= JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => //BASICALLY your Digital ID Card, this will allow users to just provide this token rather than logging in everytime to access THEIR information or information that requires authentication
+            {
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = true, //cheks that token is meant for MY APP
+                    ValidateIssuer = true, //checks that the token was issued by MY Server
+                    ValidateLifetime = true, //check if token is EXPIRED
+                    RequireExpirationTime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config["JWT:Issuer"],
+                    ValidAudience = config["JWT:Audience"],
+                    ClockSkew = TimeSpan.Zero,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JWT:Key"]!))
+                };
+            });
             return services;
         } 
 

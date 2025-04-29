e3bab6221c223d911cd607527a0c05b93f81ef53
fourcheesebagel <112727798+fourcheesebagel@users.noreply.github.com>
2025-04-29 10:27:41 -0600

    Inserting the logger into the build, it will try and attach the logger first to the build, then if it is successful, it will add Serilog to the application, before running

--- eCommerceApp.Host/Program.cs
+++ eCommerceApp.Host/Program.cs
@@ -1,7 +1,19 @@
 using eCommerceApp.Infrastructure.DependencyInjection;
 using eCommerceApp.Application.DependencyInjection;
+using Serilog;
+
+//THIS IS OUR Presentation Layer
 
 var builder = WebApplication.CreateBuilder(args);
+
+//This Logger here will create a folder, into a file called log.txt, logging info daily once this is running, so whenever this runs continuously it will daily log to this file
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("log/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+builder.Host.UseSerilog();
+Log.Logger.Information("Application is Building.....");
 
 // Add services to the container.
 
@@ -13,21 +25,32 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructureService(builder.Configuration); //Contains our DB Connection
 builder.Services.AddApplicationService();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var app = builder.Build();
+    app.UseSerilogRequestLogging();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseInfrastructureService();
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    Log.Logger.Information("Application is running........");
+
+    app.Run();
+} catch (Exception ex)
+{
+    Log.Logger.Error(ex, "Application failed to start");
+} finally
+{
+    Log.CloseAndFlush();
 }
-
-app.UseInfrastructureService();
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();

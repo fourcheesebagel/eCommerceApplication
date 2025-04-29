2aa16d863b95cae6d7cd1ebf760e391e54d564da
fourcheesebagel <112727798+fourcheesebagel@users.noreply.github.com>
2025-04-29 09:51:41 -0600

    creating sql exceptions with custom messages, middleware is checking the data that is trying to be passed from the Application layer

--- eCommerceApp.Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
+++ eCommerceApp.Infrastructure/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace eCommerceApp.Infrastructure.Middleware
 {
-    internal class ExceptionHandlingMiddleware
+    public class ExceptionHandlingMiddleware(RequestDelegate _next)
     {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            } 
+            catch (DbUpdateException ex)
+            {
+                var innerException = ex.InnerException as SqlException;
+                if (innerException != null)
+                {
+                    switch (innerException.Number)
+                    {
+                        case 2627: //Unique Constraint Violation
+                            context.Response.StatusCode = StatusCodes.Status409Conflict;
+                            await context.Response.WriteAsync("Unique Constraint Violation");
+                            break;
+                        case 515: //Cannot insert nulls
+                            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                            await context.Response.WriteAsync("Cannot insert null");
+                            break;
+                    }
+                }
+            }
+        }
     }
 }

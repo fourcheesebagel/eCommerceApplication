fd36c85f421c7a157510d1b8432feb9b1c8b3d62
fourcheesebagel <112727798+fourcheesebagel@users.noreply.github.com>
2025-06-17 10:15:11 -0600

    Added additional Cattegory Specific Services

--- eCommerceApp.Application/Services/Implementations/CategoryService.cs
+++ eCommerceApp.Application/Services/Implementations/CategoryService.cs
@@ -5,9 +5,10 @@ using eCommerceApp.Application.DTOs.Product;
 using eCommerceApp.Application.Services.Interfaces;
 using eCommerceApp.Domain.Entities;
 using eCommerceApp.Domain.Interfaces;
+using eCommerceApp.Domain.Interfaces.CategorySpecifics;
 namespace eCommerceApp.Application.Services.Implementations
 {
-    internal class CategoryService(IGeneric<Category> categoryInterface, IMapper mapper) : ICategoryService
+    internal class CategoryService(IGeneric<Category> categoryInterface, IMapper mapper, ICategory categorySpecifics) : ICategoryService
     {
         public async Task<ServiceResponse> AddAsync(CreateCategory category)
         {
@@ -39,6 +40,15 @@ namespace eCommerceApp.Application.Services.Implementations
             if (rawData == null) return new GetCategory();
 
             return mapper.Map<GetCategory>(rawData);
+        }
+
+        public async Task<IEnumerable<GetProduct>> GetProductsByCategory(Guid categoryId)
+        {
+            var products = await categorySpecifics.GetProductsByCategory(categoryId);
+            if (!products.Any())
+                return [];
+
+            return mapper.Map<IEnumerable<GetProduct>>(products);
         }
 
         public async Task<ServiceResponse> UpdateAsync(UpdateCategory category)
